814cd92e9c7c49e384dde03a52f4cbe59cea85aa
CarterGibbs1 <97470347+CarterGibbs1@users.noreply.github.com>
2022-07-27 14:36:41 -0600

    coded experiments

--- sawtooth-sdk-java/examples/election_basic/experiments/ExperimentConstants.java
+++ sawtooth-sdk-java/examples/election_basic/experiments/ExperimentConstants.java
@@ -8,7 +8,7 @@ public class ExperimentConstants {
 
     // message size
     public static final int[] NUM_CANDIDATES = {20, 40, 60, 80};
-    public static final int[] NUM_VOTERS = {5000, 10000, 15000, 20000};
+    public static final int[] NUM_VOTERS = {5000, 25000, 125000, 625000};
 
     // generating pub key
     public static final int[] numPubKeysToTest = {5, 10, 15, 20, 25};
